c1b4aa9b3cc9338faf0fcdbf0b38a53f9d8236da
Kympy <gftr9905@gmail.com>
2022-08-03 21:49:14 +0900

    카메라 무빙 수정, 트레일 렌더러 수정
    더욱 부드러운 카메라 움직임

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    public GameObject LookTarget;
+    private float smoothSpeed = 10f;
+    private Vector3 desiredPosition;
+    private Quaternion desiredRotation;
+    private Vector3 offset = new Vector3(0f, 3f, 0f);
+
+    private void Start()
+    {
+        LookTarget = GameObject.FindGameObjectWithTag("Player").gameObject;
+    }
+    private void FixedUpdate()
+    {
+        desiredPosition = LookTarget.transform.TransformPoint(0f, 3f, -20f);
+        desiredRotation = Quaternion.LookRotation(LookTarget.transform.position + offset - transform.position);
+
+        transform.position = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+        transform.rotation = Quaternion.Slerp(transform.rotation, desiredRotation, smoothSpeed * Time.deltaTime);
+    }
+}
